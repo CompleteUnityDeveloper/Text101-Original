b98be5d56000357bd821e85e5950e5878cff53aa
Ben Tristem <ben@CompleteUnityDeveloper.com>
2014-11-03 17:44:52 +0000

    Adding 2D User Interface Text

---
+++ Assets/TextController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextController : MonoBehaviour {
+
+    public Text text;
+
+	// Use this for initialization
+	void Start () {
+	    text.text = "Hello world";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			text.text = "Space key pressed";	
+		}
+	}
+}

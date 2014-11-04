aa88789774e6462eb53d350ce371fa8513f0d0d3
Ben Tristem <ben@CompleteUnityDeveloper.com>
2014-11-04 14:34:21 +0000

    Word Wrapping & Joining Strings

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -14,7 +14,10 @@ public class TextController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Space)) {
-			text.text = "Space key pressed";	
+			text.text = "You are in a prison cell, and you want to escape. There are " +
+						"some dirty sheets on the bed, a mirror on the wall, and the door " +
+						"is locked from the outside.\n\n" +
+						"Press S to view Sheets, M to view Mirror and L to view Lock" ;
 		}
 	}
 }

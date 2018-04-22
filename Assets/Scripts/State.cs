239da1a99f3c5402a8bd64dfc0fb80460ac4107e
Ben Tristem <github@bentristem.com>
2018-04-22 19:14:35 +0100

    Setting foundations

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(3, 10)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+}

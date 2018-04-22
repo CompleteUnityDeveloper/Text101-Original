c7d233142d615caa25ac8fc084808b3aadff825e
Ben Tristem <github@bentristem.com>
2018-04-22 20:07:44 +0100

    Thanks Lizzie for helping with SO port

--- Assets/Scripts/State.cs
+++ Assets/Scripts/State.cs
@@ -7,4 +7,14 @@ public class State : ScriptableObject
 {
     [TextArea(3, 10)] [SerializeField] string storyText;
     [SerializeField] State[] nextStates;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
 }
